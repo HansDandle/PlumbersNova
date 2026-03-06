8cc2c332e34b9dd41da945a061e9e7339cb8cab9
HansDandle <danshandle+2@gmail.com>
2026-03-06 01:52:12 -0600

    fixed some api routes, linux binary, added misssing prisma client

--- app/api/leads/route.ts
+++ app/api/leads/route.ts
@@ -1,4 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
+
+export const dynamic = 'force-dynamic'
 import { prisma } from '@/lib/prisma'
 import { getSessionFromRequest } from '@/lib/auth'
 import type { CreateLeadBody } from '@/types'
